49e8f2d2bfadc70a10024abb935d2e613ee11df5
sideeq12 <abdwaheed2018@gmail.com>
2023-08-15 18:50:53 +0100

    boiler plate

---
+++ src/main/java/com/alowonle/rotaryapi/RotaryApiApplication.java
@@ -0,0 +1,13 @@
+package com.alowonle.rotaryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotaryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotaryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alowonle/rotaryapi/RotaryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alowonle.rotaryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RotaryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
